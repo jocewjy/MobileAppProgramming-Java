a48e6307467b9b9a5d45150c79df6b81782be6d7
Jocelyn Wijaya <jocelyn.wijaya@student.umn.ac.id>
2021-09-07 12:16:49 +0700

    IF570EL-Week01

---
+++ week01_34804/app/src/main/java/umn/ac/id/week01_34804/MainActivity.kt
@@ -0,0 +1,11 @@
+package umn.ac.id.week01_34804
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
